9a59e6ab168e853b1192d3a0e83fbd7a90209288
Ritesh Kapoor <ritesh@workoidz.com>
2019-09-19 21:46:23 +0530

    mirror bug fixed for employee controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -65,10 +65,10 @@ class EmployeesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_store
-      @store = Store.find(params[:store_id])
+      @store = logged_in_store #Store.find(params[:store_id])
     end
     def set_employee
-      @employee = Employee.find(params[:id])
+      @employee = @store.employees.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
