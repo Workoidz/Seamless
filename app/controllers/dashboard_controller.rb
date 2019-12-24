afda9dc1851f6afb3382382337cb69ea22935303
Ritesh Kapoor <ritesh@workoidz.com>
2019-12-24 15:43:36 +0530

    changed == to = in Active record query generation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,15 +2,15 @@ class DashboardController < ApplicationController
 before_action :set_store
 
 	def today_trails
-		@trails_for_today = @store.orders.where("trail_date == '#{Time.now.to_date}'").where("trail_status == 'Not Done'")
-		@trails_delayed = @store.orders.where("trail_date < '#{Time.now.to_date}'").where("trail_status == 'Not Done'")
-		@trails_upcoming = @store.orders.where("trail_date > '#{Time.now.to_date}'").where("trail_status == 'Not Done'")
+		@trails_for_today = @store.orders.where("trail_date = '#{Time.now.to_date}'").where("trail_status = 'Not Done'")
+		@trails_delayed = @store.orders.where("trail_date < '#{Time.now.to_date}'").where("trail_status = 'Not Done'")
+		@trails_upcoming = @store.orders.where("trail_date > '#{Time.now.to_date}'").where("trail_status = 'Not Done'")
 	end
 
 	def today_deliveries
-		@deliveries_for_today = @store.orders.where("delivery_date == '#{Time.now.to_date}'").where("delivery_status == 'Not Done'")
-		@deliveries_delayed = @store.orders.where("delivery_date < '#{Time.now.to_date}'").where("delivery_status == 'Not Done'")
-		@deliveries_upcoming = @store.orders.where("delivery_date > '#{Time.now.to_date}'").where("delivery_status == 'Not Done'")
+		@deliveries_for_today = @store.orders.where("delivery_date = '#{Time.now.to_date}'").where("delivery_status = 'Not Done'")
+		@deliveries_delayed = @store.orders.where("delivery_date < '#{Time.now.to_date}'").where("delivery_status = 'Not Done'")
+		@deliveries_upcoming = @store.orders.where("delivery_date > '#{Time.now.to_date}'").where("delivery_status = 'Not Done'")
 	end
 
 
